99a08c3c075c3093ddbb49beffb7574e34c52057
Rob Thijssen <rthijssen@gmail.com>
2016-05-02 10:11:26 +0000

    github client

---
+++ minion.taskmaster/GitHub.cs
@@ -0,0 +1,16 @@
+﻿using Octokit;
+using System.Configuration;
+
+namespace minion.taskmaster
+{
+    internal class GitHub
+    {
+        public GitHub()
+        {
+            var githubProductHeader = ConfigurationManager.AppSettings.Get("githubProductHeader");
+            var githubUsername = ConfigurationManager.AppSettings.Get("githubUsername");
+            var githubPassword = ConfigurationManager.AppSettings.Get("githubPassword");
+            var client = new GitHubClient(new ProductHeaderValue(githubProductHeader)) { Credentials = new Credentials(githubUsername, githubPassword) };
+        }
+    }
+}
